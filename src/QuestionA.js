89a1ccff9b5e75765f8e29039fea7a868af4cc6c
Navpreet Singh <navpreet.singh@toptal.com>
2022-05-11 20:40:26 +0530

    Question: counter class completed

--- src/QuestionA.js
+++ src/QuestionA.js
@@ -1,94 +1,47 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
+// import './App.css';
 
-export default class QuestionA extends React.Component {
+import cx from 'classnames';
+
+export default class QuestionA extends Component {
   constructor() {
     super();
     this.state = {
-      firstName: "",
-      lastName: "",
-      isFriendly: true,
-      gender: "male",
-      favColor: "blue",
+      counter: 42,
     };
   }
 
-  handleChange = (event) => {
-    const { name, value, checked, type } = event.target;
-    type === "checkbox"
-      ? this.setState({
-          [name]: checked,
-        })
-      : this.setState({
-          [name]: value,
-        });
+  incrementCounter = (event) => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
   };
+
   render() {
     return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="firstName"
-          value={this.state.firstName}
-          placeholder="first name"
-          onChange={(e) => this.handleChange(e)}
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={this.state.lastName}
-          placeholder="last name"
-          onChange={this.handleChange}
-        />
-        <p>{`${this.state.firstName}  ${this.state.lastName}`}</p>
-        <textarea
-          type="text"
-          value={this.state.textarea}
-          placeholder="write Something about yourself"
-          onChange={this.handleChange}
-        />
-        <label>
-          <input
-            type="checkbox"
-            checked={this.state.isFriendly}
-            name="isFriendly"
-            onChange={this.handleChange}
-          />
-        </label>
-        <input
-          type="radio"
-          checked={this.state.gender === "male"}
-          name="gender"
-          value="male"
-          onChange={this.handleChange}
-        />
-        Male
-        <input
-          type="radio"
-          checked={this.state.gender === "female"}
-          name="gender"
-          value="female"
-          onChange={this.handleChange}
-        />
-        Female
-        <select
-          value={this.state.favColor}
-          onChange={this.handleChange}
-          name="favColor"
-        >
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-          <option value="red">Red</option>
-          <option value="orange">Orange</option>
-          <option value="yellow">Yellow</option>
-        </select>
-        <h1>
-          {this.state.firstName} {this.state.lastName}
-        </h1>
-        <h2>You are a {this.state.gender}</h2>
-        <h2>Your favorite color is {this.state.favColor}</h2>
-        <button>Submit</button>
-      </form>
+      <>
+        <div>
+          <h2 className="counter">Counter</h2>
+          <p>{this.state.counter}</p>
+          <button className="counter-button" onClick={this.incrementCounter}>
+            Click
+          </button>
+        </div>
+
+        <style>{`
+
+            .counter-button {
+
+            font-size: 1rem;
+
+            padding: 5px 10px;
+
+            color: #585858;
+
+            }
+
+          `}</style>
+      </>
     );
   }
 }
